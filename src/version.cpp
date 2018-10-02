e1bc440e855a3dab612e467322562b8a2d54eded
charlie <administrador@vps593.com>
2018-10-02 11:41:38 -0500

    Stable Version
    Stable Version

--- src/version.cpp
+++ src/version.cpp
@@ -11,7 +11,7 @@
 const std::string CLIENT_NAME("Ayora");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "0.14.5"
+#define CLIENT_VERSION_SUFFIX   ""
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
